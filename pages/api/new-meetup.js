88a7cf4c7ca77503340e268bb2299b307b796f04
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-08-11 19:51:55 +0700

    feat: Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -7,7 +7,21 @@ async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    //NOT MINE - sample path:
+    const client = await MongoClient.connect(
+      "mongodb+srv://maximilian:arlAapzPqFyo4xUk@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
