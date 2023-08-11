8b1d234f92d0826412c7298d0368f5d61729ebd2
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-08-11 19:39:27 +0700

    chore: Introducing API Routes

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+}
+
+export default handler;
